08babf56c5eba0b58f22a6304cd9e3482f2865e0
mitra kermanian <86522261+MitraKermanian68@users.noreply.github.com>
2023-12-25 15:14:55 +0100

    counter

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
-const colors = ["green", "red", "rgb(133,122,200", "#f15025"];
+let count = 0;
 
-const btn = document.getElementById("btn");
-const color = document.querySelector(".color");
+const btns = document.querySelectorAll(".btn");
+const value = document.querySelector("#value");
 
-btn.addEventListener("click", function () {
-  const randomNumber = getRandomNumber();
-  document.body.style.backgroundColor = colors[randomNumber];
-  color.textContent = colors[randomNumber];
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const styles = e.currentTarget.classList;
+    if (styles.contains("decrease")) {
+      count--;
+    } else if (styles.contains("increase")) {
+      count++;
+    } else {
+      count = 0;
+    }
+    if (count > 0) {
+      value.style.color = "green";
+    }
+    if (count < 0) {
+      value.style.color = "red";
+    }
+    if (count == 0) {
+      value.style.color = "black";
+    }
+    value.textContent = count;
+  });
 });
-
-function getRandomNumber() {
-  return Math.floor(Math.random() * colors.length);
-}
